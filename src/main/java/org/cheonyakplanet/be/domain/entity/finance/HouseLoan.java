2a93b5d655787e4d36acb6fec6a4db257d238850
Kim jisu <jisu0259@naver.com>
2025-06-25 22:20:57 +0900

    test: 금융 상품 관련 test #48

--- src/main/java/org/cheonyakplanet/be/domain/entity/finance/HouseLoan.java
+++ src/main/java/org/cheonyakplanet/be/domain/entity/finance/HouseLoan.java
@@ -11,11 +11,13 @@ import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 @Entity
 @Table(catalog = "planet", name = "houseloan_product")
 @Data
+@EqualsAndHashCode(callSuper = false)
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
